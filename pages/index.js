e10a0d87a116c8761b6ff3fd7fd4d4318628a564
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-08-23 13:08:17 -0600

    yes

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
-import { About } from "./components/About";
-import { Contact } from "./components/Contact";
-import { Experience } from "./components/Experience";
-import { Home } from "./components/Home";
-import { Navbar } from "./components/Navbar";
-import { Resume } from "./components/Resume";
+import { About } from "../components/About";
+import { Contact } from "../components/Contact";
+import { Experience } from "../components/Experience";
+import { Home } from "../components/Home";
+import { Navbar } from "../components/Navbar";
+import { Resume } from "../components/Resume";
 import {
   DownloadIcon,
 } from '@heroicons/react/outline';
-import { Mywork } from "./components/Mywork";
-import { Skills } from "./components/Skills";
-import { WhatIDo } from "./components/WhatIDo";
+import { Mywork } from "../components/Mywork";
+import { Skills } from "../components/Skills";
+import { WhatIDo } from "../components/WhatIDo";
 
 export default function index() {
   return (
