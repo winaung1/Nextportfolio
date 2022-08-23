ec5804498bfce94bed56b22845a008abf63f4fdd
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-08-23 12:41:59 -0600

    hkshkjfhsda

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
