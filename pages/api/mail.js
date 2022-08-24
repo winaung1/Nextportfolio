d93b6d82be3b0850e8c856c29c36648b6e5809e0
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-08-23 18:33:54 -0600

    daf

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,25 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const mail = requre('@sendgrid/mail');
-
-mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default function handler(req, res) {
-  const body = JSON.parse(req.body);
-  
-  
-  const message = `
-    FirstName: ${body.first_name}\r\n
-    LastName: ${body.last_name}\r\n
-    Phone: ${body.phone}\r\n
-    Email: ${body.email}
-  `;
 
-  const data = {
-    to: 'winaung1997@outlook.com',
-    from: 'hello@winaung.com',
-    Subject: 'Testing'
-  }
-
-  mail.send(data);
   res.status(200).json({ status: 'OK' })
 }
