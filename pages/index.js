a72f9e5eec087cc581b70915a3bf380225a73712
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-09-29 15:13:46 -0600

    done

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function index() {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
 <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Caveat+Brush&display=swap" rel="stylesheet"/>
 
       </Head>
