0549b708cd8b596ced1c573f70f2b039871f2f83
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-12-15 11:20:54 -0700

    changed the errors

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 
-import {React, useEffect} from 'react'
+import {React} from 'react'
 import NavbarContent from './NavbarContent';
 export const Navbar = ({hidden, 
   setHidden, activeLink, setActiveLink, 
