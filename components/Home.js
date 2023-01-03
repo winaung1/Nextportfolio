f188d1be457c76448bcccf40bfc639dfe410a6d8
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-01-02 19:37:05 -0700

    sending

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div id='home'>
          <div className='background-gradient'>
-            <h1 className="text-xl md:text-3xl lg:text-5xl px-5">FRONTEND WEB DEVELOPER</h1>
+            <h1 className="text-xl md:text-3xl lg:text-5xl px-5">OK</h1>
           </div>
     </div>
   )
